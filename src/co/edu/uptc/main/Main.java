dadd0577d548f9c5bd5030bc95a62d5767926a57
Andrea1411-coder <andrea.correa02@uptc.edu.co>
2025-04-08 15:44:47 -0500

    modifique el main

--- src/co/edu/uptc/main/Main.java
+++ src/co/edu/uptc/main/Main.java
@@ -5,5 +5,6 @@ import co.edu.uptc.view.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Login login = new Login();
+        LoginView loginView = new LoginView(login);
     }
 }
