c5dc32cfb4953f696e8a74db622ed3aaae61a6fe
Santi-Rincon <santi.rincon@uptc.edu.co>
2025-04-08 16:43:34 -0500

    HOLA ARIAS

--- src/co/edu/uptc/main/Main.java
+++ src/co/edu/uptc/main/Main.java
@@ -5,5 +5,7 @@ import co.edu.uptc.view.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Login login = new Login();
+        //Hola arias 
+        System.out.println("Hola Arias");
     }
 }
