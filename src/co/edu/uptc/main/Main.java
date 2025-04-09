88a41bf58dd6749f5f08747940e1c5a3ab8f8017
Andrea Correa <andrea.correa02@uptc.edu.co>
2025-04-08 20:08:51 -0500

    Update Main.java

--- src/co/edu/uptc/main/Main.java
+++ src/co/edu/uptc/main/Main.java
@@ -5,7 +5,5 @@ import co.edu.uptc.view.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Login login = new Login();
-        //Hola arias 
-        System.out.println("Hola Nixon");
     }
 }
